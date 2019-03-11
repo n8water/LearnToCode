76c28a53df8c56447233fd8a802f3f8b619da982
Neskaya <39946966+n8water@users.noreply.github.com>
2019-03-11 14:28:19 +0100

    Created_Folders
    Trackfolders for exercism.io created manually
    
    Added acronym exercise

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string input = string.Empty;
+        string result = string.Empty;
+        string[] words = new string[]{ };
+        
+        foreach (char c in phrase)
+        {
+            if ((c == '-') | (c == '_'))
+            {
+                input += " ";
+            }
+            else
+            {
+                input += c;
+            }
+        }
+
+        words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+
+            result += words[i][0];
+        }
+
+        result = result.ToUpper();
+
+        return result;
+    }
+}
