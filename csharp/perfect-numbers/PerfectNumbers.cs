7c94c770302089f322f6f554ff78926db9292289
Neskaya <39946966+n8water@users.noreply.github.com>
2019-03-12 16:29:47 +0100

    Local Exercises uploaded
    Added the exercises:
    gigasecond, perfect number, leap

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        int temp = number;
+        int i = 1;
+        int sum = 0;
+
+        if (number < 0 | number == 0)
+            throw new ArgumentOutOfRangeException("Not a natural number");
+        
+
+        while(i<=number/2)
+        {
+            if(temp % i == 0)
+            {
+                if (temp == number)
+                    sum += i;
+                else
+                {
+                    temp = temp / i;
+                    sum += temp + i;
+                }
+            }          
+            i++;
+        };
+
+        if (sum == number)
+            return Classification.Perfect;
+        else if (sum < number)
+            return Classification.Deficient;
+        else
+            return Classification.Abundant;        
+    }
+}
