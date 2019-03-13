3d03b6deb00b1df653f78211f5be7fa6ff098ff7
Neskaya <39946966+n8water@users.noreply.github.com>
2019-03-13 11:38:43 +0100

    First working Solution
    Needs refactoring, because too much DRY violations.

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -4,6 +4,48 @@ public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string dirty = string.Empty;
+        string clean = string.Empty;
+
+        foreach (char c in phoneNumber)
+        {
+            if (char.IsDigit(c))
+                dirty += c;
+        }
+
+        if ((dirty.Length == 10)
+            && (dirty[0] != '0')
+            && (dirty[0] != '1')
+            && (dirty[3] != '0')
+            && (dirty[3] != '1'))
+        {
+            clean = dirty;
+        }
+        else if ((dirty[0] == '1')
+                 && (dirty.Length == 11)
+                 && (dirty[1] != '0')
+                 && (dirty[1] != '1')
+                 && (dirty[4] != '0')
+                 && (dirty[4] != '1'))
+        {
+            clean = dirty.Substring(1);
+        }
+        else if (dirty.Length < 10)
+        {
+            throw new ArgumentException("phone number to short");
+        }
+        else
+        {
+            throw new ArgumentException("phone number too long");
+        }
+
+        return clean;
+    }
+
+    private static bool IsValid(string number)
+    {
+        bool isValid = false;
+
+        return isValid;
     }
 }
