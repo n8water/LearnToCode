95dfad22e5dcb49786c6cb6078c7e59d303e5fee
Neskaya <39946966+n8water@users.noreply.github.com>
2019-03-14 11:16:47 +0100

    Refactoring in process

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -4,48 +4,62 @@ public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        string dirty = string.Empty;
+        string toCheck = string.Empty;
         string clean = string.Empty;
 
         foreach (char c in phoneNumber)
         {
             if (char.IsDigit(c))
-                dirty += c;
+                toCheck += c;
         }
 
-        if ((dirty.Length == 10)
-            && (dirty[0] != '0')
-            && (dirty[0] != '1')
-            && (dirty[3] != '0')
-            && (dirty[3] != '1'))
-        {
-            clean = dirty;
-        }
-        else if ((dirty[0] == '1')
-                 && (dirty.Length == 11)
-                 && (dirty[1] != '0')
-                 && (dirty[1] != '1')
-                 && (dirty[4] != '0')
-                 && (dirty[4] != '1'))
-        {
-            clean = dirty.Substring(1);
-        }
-        else if (dirty.Length < 10)
-        {
-            throw new ArgumentException("phone number to short");
-        }
+        //Check length
+        if(toCheck.Length < 12 && toCheck.Length > 9)
+            CheckValidity(toCheck);
         else
         {
-            throw new ArgumentException("phone number too long");
+            throw new ArgumentException("Phone number length is invalid");
         }
+
+        #region Quick and dirty solution
+        //Quick and dirty solution
+        //if ((dirty.Length == 10)
+        //    && (dirty[0] != '0')
+        //    && (dirty[0] != '1')
+        //    && (dirty[3] != '0')
+        //    && (dirty[3] != '1'))
+        //{
+        //    clean = dirty;
+        //}
+        //else if ((dirty[0] == '1')
+        //         && (dirty.Length == 11)
+        //         && (dirty[1] != '0')
+        //         && (dirty[1] != '1')
+        //         && (dirty[4] != '0')
+        //         && (dirty[4] != '1'))
+        //{
+        //    clean = dirty.Substring(1);
+        //}
+        //else if (dirty.Length < 10)
+        //{
+        //    throw new ArgumentException("phone number to short");
+        //}
+        //else
+        //{
+        //    throw new ArgumentException("phone number too long");
+        //} 
+        #endregion
 
         return clean;
     }
 
-    private static bool IsValid(string number)
+    private static void CheckValidity(string numberToCheck)
     {
-        bool isValid = false;
+        if (numberToCheck.Length == 11 && numberToCheck[0] == '1')
+            numberToCheck = numberToCheck.Substring(1);
 
-        return isValid;
+
+
     }
+
 }
