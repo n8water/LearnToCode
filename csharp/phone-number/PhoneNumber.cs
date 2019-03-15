5e09467c7a228777f4bc24cdcf921760881e8975
Neskaya <39946966+n8water@users.noreply.github.com>
2019-03-15 09:07:33 +0100

    Finally submitted code
    Working and submitted

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,7 +1,7 @@
 using System;
 
 public class PhoneNumber
-{   
+{
     public static string Clean(string phoneNumber)
     {
         string toCheck = string.Empty;
@@ -19,58 +19,32 @@ public class PhoneNumber
         {
             throw new ArgumentException("Phone number length is invalid");
         }
-
-        #region Quick and dirty solution
-        //Quick and dirty solution
-        //if ((dirty.Length == 10)
-        //    && (dirty[0] != '0')
-        //    && (dirty[0] != '1')
-        //    && (dirty[3] != '0')
-        //    && (dirty[3] != '1'))
-        //{
-        //    clean = dirty;
-        //}
-        //else if ((dirty[0] == '1')
-        //         && (dirty.Length == 11)
-        //         && (dirty[1] != '0')
-        //         && (dirty[1] != '1')
-        //         && (dirty[4] != '0')
-        //         && (dirty[4] != '1'))
-        //{
-        //    clean = dirty.Substring(1);
-        //}
-        //else if (dirty.Length < 10)
-        //{
-        //    throw new ArgumentException("phone number to short");
-        //}
-        //else
-        //{
-        //    throw new ArgumentException("phone number too long");
-        //} 
-        #endregion
-
+       
         return clean;
     }
 
     private static string CheckValidity(string numberToCheck)
     {
-        string clean = string.Empty;
+        string validNumber = string.Empty;
 
         if (numberToCheck.Length == 11 && numberToCheck[0] == '1')
             numberToCheck = numberToCheck.Substring(1);
-        else
-            throw new ArgumentException();
+        else if(numberToCheck.Length == 11)
+            throw new ArgumentException("Number is invalid");
 
 
-        if ((numberToCheck[0] != '0') && (numberToCheck[3] != '1'))
+        if ((numberToCheck[0] != '0') &&
+            (numberToCheck[0] != '1') &&
+            (numberToCheck[3] != '0') &&
+            (numberToCheck[3] != '1'))
         {
-            clean = numberToCheck;
+            validNumber = numberToCheck;
         }
         else
         {
             throw new ArgumentException();
         }
 
-        return clean;
+        return validNumber;
     }
 }
