fb4c914b172037508c50fb11f39d3235f694e5e3
Neskaya <39946966+n8water@users.noreply.github.com>
2019-04-01 15:13:34 +0200

    Submitted Sum-Of-Multiples
    Including an alternative method from other user

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sum = 0;
+
+        List<int> numbersToSumUp = new List<int>();
+        foreach (int number in multiples)
+        {
+            if (number != 0)
+                for (int i = 1; i < max; i++)
+                {
+                    if (i % number == 0 && !numbersToSumUp.Contains(i))
+                    {
+                        numbersToSumUp.Add(i);
+                        sum += i;
+                    }
+                }
+        }        
+        return sum;
+
+        //Alternative von bgraf's solution on exercism.io
+            
+        //=> Enumerable
+        //    .Range(1, max - 1)
+        //    .Where(i => multiples.Any(j => j > 0 && i % j == 0))
+        //    .Sum();
+
+
     }
 }
+
