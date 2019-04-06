f63c1bcc5fd3030dc7cda87edffd0d12317f79a5
n8water <39946966+n8water@users.noreply.github.com>
2019-04-06 13:17:17 +0200

    Changes
    Series submitted version
    
    Next core nucleotide count added

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -6,24 +6,8 @@ public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        //working solution
-        //List<string> slices = new List<string>();
-        //int runs = numbers.Length - sliceLength;
-
         if (numbers.Length < sliceLength || sliceLength<=0)
             throw new ArgumentException("Invalid input");
-
-        //else
-        //{
-        //    for (int i = 0; i <= runs; i++)
-        //    {
-        //        slices.Add(numbers.Substring(i, sliceLength));
-        //    }
-        //}
-
-        //return slices.ToArray();
-
-        //Alternative with Linq
        
         var slices = numbers.Select((x,i) => i<=(numbers.Length-sliceLength) && (i+sliceLength)<=numbers.Length ? numbers.Substring(i, sliceLength): string.Empty);
         var validSlices = slices.Where(x => x.Length>0).Select(x => x);         
