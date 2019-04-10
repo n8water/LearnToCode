b5ca91f48604d4a7881261dbb12d9dae97caf11e
Neskaya <39946966+n8water@users.noreply.github.com>
2019-04-10 21:06:21 +0200

    RobotName started

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 public class Robot
 {
-    public string Name
+    public Robot()
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        
     }
+
+    public string Name { get; private set; }
+
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var Name = new RobotNames();
+        Name.GenerateName();
+    }
+
+
+}
+
+public class RobotNames
+{
+    public List<string> robotNames = new List<string>();
+
+    public string GenerateName()
+    {
+        string name = string.Empty;
+        Random random = new Random();
+        
+        for (int i = 0; i < 3; i++)
+        {
+            if(i<2)
+                name += (char) random.Next(65,91);
+            else
+                name += random.Next(100,1000);
+        }
+
+        if(!robotNames.Contains(name))
+        {
+            robotNames.Add(name);
+            return name;
+        }
+            
+        else               
+            throw new Exception ("This Name already exists");    
     }
 }
