c8fba72d91092f86ba6b575dec0c47689d0ffd0b
n8water <39946966+n8water@users.noreply.github.com>
2019-04-10 21:42:22 +0200

    Update ArmstrongNumbers.cs
    Just opend it

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -4,6 +4,7 @@ public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
+
         throw new NotImplementedException("You need to implement this function.");
     }
 }
