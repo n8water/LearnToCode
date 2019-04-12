910af38b699e2e683c19b7416e4c1763ebca2530
Neskaya <39946966+n8water@users.noreply.github.com>
2019-04-12 16:56:09 +0200

    Update ArmstrongNumbers.cs
    Submitted Version

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -8,43 +8,6 @@ public static class ArmstrongNumbers
 
     public static bool IsArmstrongNumber(int number)
     {
-        //bool _isArmstrongNumber = false;
-        //List<int> digits = new List<int>();
-        //int exponent = 0;
-        //int temporaryNumber = number;
-
-        //if(temporaryNumber<0)
-        //    throw new ArgumentException();
-
-        //while (temporaryNumber>0)
-        //{        
-        //    if(temporaryNumber > 9)
-        //    {
-        //        digits.Add(temporaryNumber%10);
-        //        temporaryNumber = temporaryNumber / 10;            
-        //    }
-        //    else
-        //    {
-        //        digits.Add(temporaryNumber);
-        //        temporaryNumber = 0;
-        //    }
-        //}
-
-        //exponent = digits.Count;
-
-        //foreach (int digit in digits)
-        //{
-        //    temporaryNumber += (int) Math.Pow(digit, exponent);
-        //}
-
-        //if(temporaryNumber == number)
-        //    _isArmstrongNumber = true;
-
-        //return _isArmstrongNumber;   
-
-
-        //Second attempt
-
         int temp = number;
         int exponent = (int)number.ToString().Length;
         int result = 0;
