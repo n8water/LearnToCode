e1734e0cc175bc474d7e2dabc447285f268749b6
Neskaya <39946966+n8water@users.noreply.github.com>
2019-04-12 16:54:50 +0200

    ArmstrongNumber
    with the first 2 attempts.
    before submitting to Exercism.io

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ArmstrongNumbers
 {
+
+
     public static bool IsArmstrongNumber(int number)
     {
+        //bool _isArmstrongNumber = false;
+        //List<int> digits = new List<int>();
+        //int exponent = 0;
+        //int temporaryNumber = number;
 
-        throw new NotImplementedException("You need to implement this function.");
+        //if(temporaryNumber<0)
+        //    throw new ArgumentException();
+
+        //while (temporaryNumber>0)
+        //{        
+        //    if(temporaryNumber > 9)
+        //    {
+        //        digits.Add(temporaryNumber%10);
+        //        temporaryNumber = temporaryNumber / 10;            
+        //    }
+        //    else
+        //    {
+        //        digits.Add(temporaryNumber);
+        //        temporaryNumber = 0;
+        //    }
+        //}
+
+        //exponent = digits.Count;
+
+        //foreach (int digit in digits)
+        //{
+        //    temporaryNumber += (int) Math.Pow(digit, exponent);
+        //}
+
+        //if(temporaryNumber == number)
+        //    _isArmstrongNumber = true;
+
+        //return _isArmstrongNumber;   
+
+
+        //Second attempt
+
+        int temp = number;
+        int exponent = (int)number.ToString().Length;
+        int result = 0;
+
+        while (temp>0)
+        {
+            result += (int) Math.Pow(temp%10, exponent);
+            temp /= 10;
+        }
+
+        return result == number;
     }
 }
