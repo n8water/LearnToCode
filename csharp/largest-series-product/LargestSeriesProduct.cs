899541e5f8e3de7d1c56719a7ac9fbb0ce9e8a47
Neskaya <39946966+n8water@users.noreply.github.com>
2019-04-22 22:59:49 +0200

    LargesSeriesProduct in progress
    Trying to figure it out, first unsuccessfull try

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        long result = 0;
+        List<long> segments = new List<long>();
+        long temp = 1;
+
+        if(string.IsNullOrEmpty(digits) || (digits.Length < span) || span <= 0)
+            throw new ArgumentException();
+        
+        for (int i = 0; i <= digits.Length-span; i++)
+        {
+            if(!digits.Substring(i,span).Contains('0'))
+            {
+                segments.Add(Convert.ToInt64(digits.Substring(i,span)));
+            }
+        }
+
+        
+
+
+        return result;
     }
 }
