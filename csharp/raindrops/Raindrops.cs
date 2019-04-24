379bb6915ee1be493492ca7ce2932e5645955b60
n8water <39946966+n8water@users.noreply.github.com>
2019-04-24 14:44:46 +0200

    Raindrops solved and submitted

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string result = string.Empty;
+        Dictionary<int, string> factors = new Dictionary<int, string>
+            {
+                {3,"Pling"},
+                {5,"Plang"},
+                {7,"Plong"},
+            };
+
+        foreach (var key in factors.Keys)
+        {
+            if (number % key == 0)
+                result += factors[key];
+        }
+
+        if (result == string.Empty)
+            result = number.ToString();
+        
+        return result;
     }
 }
