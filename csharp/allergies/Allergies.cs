81dbce9a7de6ed42c3f2852d74a8613a8bd50ba6
n8water <39946966+n8water@users.noreply.github.com>
2019-04-25 23:44:57 +0200

    Allergies in progress
    3rd test failed

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -2,7 +2,7 @@ using System;
 
 public enum Allergen
 {
-    Eggs,
+    Eggs = 1,
     Peanuts,
     Shellfish,
     Strawberries,
@@ -14,17 +14,49 @@ public enum Allergen
 
 public class Allergies
 {
+    private int _mask;
+
+    
     public Allergies(int mask)
     {
+        _mask = mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int allergenValue = (int)allergen;
+        bool isAlleric = false;
+
+        int test =  _mask & allergenValue;
+        if (test > 0)
+            isAlleric = true;
+
+        //int all = 3;
+        //Allergen aller = (Allergen)all;
+
+        return isAlleric;
     }
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Allergen[] result = new Allergen[8];
+
+        int temp = 0;
+
+
+        //if (temp % 2 != 0)
+        //{
+        //    result[0] = (Allergen)1;
+        //    temp -= 1;
+        //}
+
+        for (int i = 0; Math.Pow(2,i) <= _mask; i++)
+        {
+            temp = _mask & (int)Math.Pow(2, i);
+            if (temp > 0)
+                result[i] = (Allergen)i;
+        }
+
+        return result.Length > 0 ? result : throw new ArgumentException();
     }
 }
