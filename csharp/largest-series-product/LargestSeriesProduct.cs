fefd1097ed173954a21c9d199eed51ceb65a141b
n8water <39946966+n8water@users.noreply.github.com>
2019-04-30 23:29:55 +0200

    LargesSeriesProducts in progress
    Work in  progress, still 2 tests failing

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -7,16 +7,48 @@ public static class LargestSeriesProduct
     public static long GetLargestProduct(string digits, int span) 
     {
         long largestProduct = 0;
+        long temp = 1;
+        List<string> slices = new List<string>();
 
-        if (digits.Length <= 0 || span <= 0)
+        if (digits.Length <= 0 || span <= 0 || span > digits.Length)
             throw new ArgumentException();
 
-        List<char> input = digits.ToList();
-
-        for (int i = 0; i+span <= input.Count; i++)
+        for (int i = 0; i + span <= digits.Length; i++)
         {
-            
+            slices.Add(digits.Substring(i, span));
         }
 
+        foreach  (string slice in slices)
+        {
+            if (slice.Contains('0'))
+                continue;
+
+            try
+            {
+                long tempDigits = Convert.ToInt64(slice);
+                while (tempDigits >= 10)
+                {
+                    temp *= tempDigits % 10;
+                    tempDigits = tempDigits / 10;
+                }
+                temp *= tempDigits;
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException();
+            }
+                        
+            if (temp > largestProduct)
+            {
+                largestProduct = temp;
+                temp = 1;
+            }
+            else
+            {
+                temp = 1;
+            }
+        }
+
+        return largestProduct;
     }
 }
