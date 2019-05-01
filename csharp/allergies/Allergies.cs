1c1e8c302bd305adc5afac237fb63557ef894c9b
Neskaya <39946966+n8water@users.noreply.github.com>
2019-05-01 13:23:30 +0200

    Update Allergies.cs
    Try to implement the discussion point from mike (mentor)

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -1,59 +1,80 @@
 using System;
 using System.Collections.Generic;
 
+[Flags]
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs           = 1 << 0,
+    Peanuts        = 1 << 1,
+    Shellfish      = 1 << 2,
+    Strawberries   = 1 << 3,
+    Tomatoes       = 1 << 4,
+    Chocolate      = 1 << 5,
+    Pollen         = 1 << 6,
+    Cats           = 1 << 7,
 }
 
 public class Allergies
 {
-    private int _mask;
-    private List<Allergen> allergenList;
-    
-    
+    private int maskValue;
     public Allergies(int mask)
     {
-        _mask = mask;
+        maskValue = mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-        int allergenValue = (int)Math.Pow(2, (double)allergen);
-        bool isAlleric = false;
 
-        int test =  _mask & allergenValue;
-        if (test > 0)
-            isAlleric = true;
-        
-        return isAlleric;
     }
 
     public Allergen[] List()
     {
-        int tempAllergen;
-        allergenList = new List<Allergen>();
 
-        if (_mask % 2 != 0)
-        {
-            allergenList.Add((Allergen)0);
-        }
-
-        for (int i = 1; i <= _mask; i++)
-        {
-            tempAllergen = _mask & (int)Math.Pow(2,i);
-            if(tempAllergen > 0)
-                if(i<8)
-                    allergenList.Add((Allergen)i);
-        }
-        
-        return allergenList.ToArray();
     }
 }
+
+//First working Solution
+//public class Allergies
+//{
+//    private int _mask;
+//    private List<Allergen> allergenList;
+
+
+//    public Allergies(int mask)
+//    {
+//        _mask = mask;
+//    }
+
+//    public bool IsAllergicTo(Allergen allergen)
+//    {
+//        int allergenValue = (int)Math.Pow(2, (double)allergen);
+//        bool isAlleric = false;
+
+//        int test =  _mask & allergenValue;
+//        if (test > 0)
+//            isAlleric = true;
+
+//        return isAlleric;
+//    }
+
+//    public Allergen[] List()
+//    {
+//        int tempAllergen;
+//        allergenList = new List<Allergen>();
+
+//        if (_mask % 2 != 0)
+//        {
+//            allergenList.Add((Allergen)0);
+//        }
+
+//        for (int i = 1; i <= _mask; i++)
+//        {
+//            tempAllergen = _mask & (int)Math.Pow(2,i);
+//            if(tempAllergen > 0)
+//                if(i<8)
+//                    allergenList.Add((Allergen)i);
+//        }
+
+//        return allergenList.ToArray();
+//    }
+//}
