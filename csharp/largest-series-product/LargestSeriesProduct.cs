55e224a902ad708be4cdb8c169d11822e178da86
Neskaya <39946966+n8water@users.noreply.github.com>
2019-05-01 22:39:19 +0200

    Update LargestSeriesProduct.cs
    submitted

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -8,9 +8,16 @@ public static class LargestSeriesProduct
     {
         long largestProduct = 0;
         long temp = 1;
+        long tempDigits;
         List<string> slices = new List<string>();
 
-        if (digits.Length <= 0 || span <= 0 || span > digits.Length)
+        if (digits.Length < 0 || span < 0 || span > digits.Length)
+            throw new ArgumentException();
+
+        if(digits.Length == 0 && span == 0 || span == 0)
+            return 1;
+
+        if(!double.TryParse(digits, out double result))
             throw new ArgumentException();
 
         for (int i = 0; i + span <= digits.Length; i++)
@@ -19,25 +26,19 @@ public static class LargestSeriesProduct
         }
 
         foreach  (string slice in slices)
-        {
+        {            
             if (slice.Contains('0'))
                 continue;
 
-            try
-            {
-                long tempDigits = Convert.ToInt64(slice);
-                while (tempDigits >= 10)
-                {
-                    temp *= tempDigits % 10;
-                    tempDigits = tempDigits / 10;
-                }
-                temp *= tempDigits;
-            }
-            catch (Exception)
-            {
-                throw new ArgumentException();
-            }
+            tempDigits = Convert.ToInt64(slice);
                         
+            while (tempDigits >= 10)
+            {
+                temp *= tempDigits % 10;
+                tempDigits = tempDigits / 10;
+            }
+            temp *= tempDigits;
+
             if (temp > largestProduct)
             {
                 largestProduct = temp;
