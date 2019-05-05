86ad6dc1f294128cd543972cf70da9a13fb1d9bf
Neskaya <39946966+n8water@users.noreply.github.com>
2019-05-05 22:03:29 +0200

    PrimeFactors submitted

--- csharp/prime-factors/PrimeFactors.cs
+++ csharp/prime-factors/PrimeFactors.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 public static class PrimeFactors
 {
     public static int[] Factors(long number)
     {
-        throw new NotImplementedException();
+        List<int> result = new List<int>();
+        int factor = 2;
+
+        while(number != 1)
+        {
+            if(number % factor == 0)
+            {
+                result.Add(factor);
+                number /=  factor;
+            }
+            else
+                factor++;
+        }
+
+        return result.ToArray();
     }
 }
