9508a85865b43731e4c1ee06922dc85e249fac54
Neskaya <39946966+n8water@users.noreply.github.com>
2019-05-05 21:53:00 +0200

    PythagoreanTriplet started
    started but by no means on a way to a solution

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,6 +5,13 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int a;
+        int b;
+        int c;
+        int sumAbc;
+        
+
+
+        return new List<(int a, int b, int c)>();
     }
 }
