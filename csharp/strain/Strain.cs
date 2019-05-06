3fcae7377ad0f398b887ea1bd0405b41244820cf
n8water <39946966+n8water@users.noreply.github.com>
2019-05-06 18:42:17 +0200

    Strain working and ready to submit

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -1,15 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Metadata;
 
 public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CheckPredicate(collection, predicate, "keep");
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CheckPredicate(collection, predicate, "");
     }
+
+    private static IEnumerable<T> CheckPredicate<T>(IEnumerable<T> collection, Func<T, bool> predicate, string callingMethod)
+    {
+        List<T> keep = new List<T>();
+        List<T> discard = new List<T>();
+
+        foreach (var item in collection)
+        {
+            if (predicate(item))
+                keep.Add(item);
+            else
+                discard.Add(item);
+        }
+
+        if (callingMethod == "keep")
+            return keep;
+        else
+            return discard;
+    }
+
+
+    //private static IEnumerable<T> GetCollection<T>(IEnumerable<T> collection, Func<T, bool> predicate)
+    //{
+    //    var predicateCollection = collection.Where()
+    //}
 }
