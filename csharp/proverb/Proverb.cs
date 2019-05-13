7a6d601e46eeccc33768e7623bb76536f6528d16
n8water <39946966+n8water@users.noreply.github.com>
2019-05-13 18:38:37 +0200

    Proverb to submit

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Proverb
 {
     public static string[] Recite(string[] subjects)
     {
-        throw new NotImplementedException();
+        List<string> recite = new List<string>();
+        if (subjects.Length <= 0)
+            return recite.ToArray();
+
+        for (int i = 0; i+1 < subjects.Length; i++)
+        {
+            recite.Add($"For want of a {subjects[i]} the {subjects[i + 1]} was lost.");
+        }
+        recite.Add($"And all for the want of a {subjects[0]}.");
+
+        return recite.ToArray();
     }
 }
