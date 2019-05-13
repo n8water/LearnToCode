56bb572b362022fb795abae20f3638a456818bab
n8water <39946966+n8water@users.noreply.github.com>
2019-05-13 19:29:04 +0200

    RotationalCiper
    Most tests working,
    primitive part solution, needs more work

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -4,6 +4,39 @@ public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string rotate = string.Empty;
+
+        foreach (char c in text)
+        {
+            int temp = (int)c;
+
+            //Gross und Kleinbuchstaben getrennt behandeln sonst können die Zeichen von gross zu klein wechseln
+            //das ist nicht erwünscht
+            
+
+            if ((temp <= 122 && temp >= 97) || (temp <= 90 && temp >= 65))
+            {
+                temp += shiftKey;
+                if ((temp <= 122 && temp >= 97) || (temp <= 90 && temp >=65))
+                {
+                    rotate += (char)temp;
+                    temp = 0;
+                }
+                else
+                {
+                    temp -= 26;
+                    rotate += (char)temp;
+                    temp = 0;
+                }
+
+            }
+            else
+            {
+                rotate += c;
+                temp = 0;
+            }
+        }
+
+        return rotate;
     }
 }
