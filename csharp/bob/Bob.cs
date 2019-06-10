30181e72156da55b5ca5e7ae69ee874175434361
Neskaya <39946966+n8water@users.noreply.github.com>
2019-06-10 12:51:31 +0200

    working on AllYourBase
    tricky

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -4,6 +4,15 @@ public static class Bob
 {
     public static string Response(string statement)
     {
+        //var punctuation = statement.ToCharArray();
+
+        var punctuation = statement.Substring(statement.Length-2);
+
+        switch (punctuation)
+        {
+            default:
+                break;
+        }
         throw new NotImplementedException("You need to implement this function.");
     }
 }
