d1847c7e12ed3725e4a0dd877434696d1d5c3102
Neskaya <39946966+n8water@users.noreply.github.com>
2019-07-09 21:58:56 +0200

    first version submitted

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 
 public static class MatchingBrackets
 {
+    static string openingBrackets = "([{";
+    static string closingBrackets = ")]}";
+    static Stack<char> inputBrackets = new Stack<char>();
+    static bool isPaired;
+
     public static bool IsPaired(string input)
     {
-        private static readonly Dictionary<char, char> brackets = new Dictionary<char, char>()
-        {
-            '(' , ')';
-        
-        };
-        
+        inputBrackets.Clear();
+        isPaired = false;
 
-        if(input.Length == 0)
+        if(input == string.Empty)
+            isPaired = true;
+        else
+        {
+            foreach (char c in input)
+            {
+                if (openingBrackets.Contains(c))
+                {
+                    inputBrackets.Push(c);
+                }
+                else if (closingBrackets.Contains(c))
+                {
+                    isPaired = checkIfBracketsPair(c);
+                }
+            }
+        }        
+
+        if(inputBrackets.Count != 0)
+            isPaired = false;
+        
+        return isPaired;
+    }
+
+    private static bool checkIfBracketsPair(char c)
+    {
+        if(inputBrackets.Count > 0 && (openingBrackets.IndexOf(inputBrackets.Pop()) == closingBrackets.IndexOf(c)))
+        {
             return true;
+        }
         else
             return false;
     }
