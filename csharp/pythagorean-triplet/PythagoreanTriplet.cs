5125f366014046f3915a110cc7a6275223ee691c
neskaya <n8water@gmail.com>
2019-10-08 11:50:15 +0200

    working but slow on large numbers

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,13 +5,46 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        int a;
-        int b;
-        int c;
+        int a = 1;
+        int b = sum;
+        int c = sum;
         int sumAbc;
-        
+        var result = new List<(int a, int b, int c)>();
 
+        for (int ia = a; ia < sum / 3; ia++)
+        {
+            for (int ib = ia + 1; ib <b; ib++)
+            {
+                for (int ic = ib + 1; ic < c; ic++)
+                {
+                    if (ia + ib + ic == sum && (ia * ia + ib * ib == ic * ic))
+                    {
+                        result.Add((ia, ib, ic));
+                        a = ia;
+                        b = ib;
+                        c = ic;
+                    }
 
-        return new List<(int a, int b, int c)>();
+                }
+            }
+        }
+
+        //for (int ia = sum / 25; ia < sum / 3; ia++)
+        //{
+        //    for (int ib = ia + 1; ib < sum / 2; ib++)
+        //    {
+        //        for (int ic = ib + 1; ic < sum / 2; ic++)
+        //        {
+        //            if (ia + ib + ic == sum && (ia * ia + ib * ib == ic * ic))
+        //            {
+        //                result.Add((ia, ib, ic));
+        //            }
+
+        //        }
+        //    }
+        //}
+
+        return result;
+        //return new List<(int a, int b, int c)>();
     }
 }
