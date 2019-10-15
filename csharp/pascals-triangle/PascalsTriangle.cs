aa8f412e11af5dfaf4f20b91f649264958f86404
neskaya <n8water@gmail.com>
2019-10-15 10:45:19 +0200

    solved

--- csharp/pascals-triangle/PascalsTriangle.cs
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -7,60 +7,34 @@ public static class PascalsTriangle
     public static IEnumerable<IEnumerable<int>> Calculate(int rows)
     {
         int[][] result = new int[rows][];
-
-        int test = result.Length;
+        var rowNumbers = new List<int>();
 
         if (rows <= 0)
            return result;
 
         for (int n = 0; n < rows; n++)
+        {
+            rowNumbers.Clear();
+
             if (n == 0)
             {
-                int number = n + 1 ;
-                result[0] = new int[] { 1 };
+                rowNumbers.Add(1);
             }
-            else if (n == 1)
-                result[n] = new int[] { n, n };
+            else
+            {
+                int previousRow = n - 1;
+                int columns = n + 1;
 
-        //var result = new int[0];
-
-        //if (rows <= 0)
-        //    yield return int[0];
-
-        //for (int n = 0; n < rows; n++)
-        //    if (n == 0)
-        //        result.Add(new int[n]);
-        //    //for(int k = 0; k <= n; k++)
-
-
-
-
-
-
-        //result.Add(new List<int>());
-
+                foreach (int k in Enumerable.Range(0, columns))
+                {
+                    if (k - 1 < 0 || k > previousRow)
+                        rowNumbers.Add(1);
+                    else
+                        rowNumbers.Add(result[previousRow][k] + result[previousRow][k - 1]);
+                }
+            }
+            result[n] = rowNumbers.ToArray();
+        }
         return result;
-        //throw new NotImplementedException();
     }
-
 }
-
-/* Notizen
-Pascalsches Dreieck
-
-    \binom{n+1}{k+1} = \binom{n}{k} + \binom{n}{k+1}
-
-Dabei kann die Variable n als Zeilenindex und k als Spaltenindex interpretiert werden, 
-wobei die Zählung mit Null beginnt (also erste Zeile n=0, erste Spalte k=0). 
-Beginnt man an den Rändern mit Einträgen mit dem Wert 1, so ergeben sich dadurch genau 
-die Binomialkoeffizienten.
-
-Die Zeilen
-Die Summe der Einträge einer Zeile wird als Zeilensumme bezeichnet. Von oben nach unten 
-verdoppeln sich die Zeilensummen von Zeile zu Zeile. Dies rührt vom Bildungsgesetz des 
-pascalschen Dreiecks her. Jeder Eintrag einer Zeile wird in der folgenden Zeile zur 
-Berechnung zweier Einträge verwendet. Hierbei muss man das Bildungsgesetz durch das 
-Hinzufügen von gedachten Nullen links und rechts von jeder Zeile verallgemeinern, so 
-dass auch die äußeren Einsen jeder Zeile durch die Addition der darüberliegenden Einträge 
-generiert werden. Da die Zeilensumme der ersten Zeile gleich eins ist, ist die Zeilensumme 
-der n-ten Zeile gleich 2^{n-1}. */
