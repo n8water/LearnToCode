8e14a51485237e112a9cb8a4d0a2c70cd54b4384
neskaya <n8water@gmail.com>
2019-10-17 08:05:34 +0200

    Update House.cs

--- csharp/house/House.cs
+++ csharp/house/House.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class House
 {
+    private static readonly Dictionary<string, string> versValues = new Dictionary<string, string>
+    {
+        { "malt", "lay"},
+        { "rat", "ate" },
+        { "cat", "killed" },
+        { "dog", "worried" },
+        { "cow with the crumpled horn", "tossed" },
+        { "maiden all forlorn", "milked" },
+        { "man all tattered and torn", "kissed" },
+        { "priest all shaven and shorn", "married" },
+        { "rooster that crowed in the morn", "woke" },
+        { "farmer sowing his corn", "kept" },
+        { "horse and the hound and the horn", "belonged" },
+    };
     public static string Recite(int verseNumber)
     {
+        return Verse(verseNumber);
         throw new NotImplementedException("You need to implement this function.");
     }
 
     public static string Recite(int startVerse, int endVerse)
     {
         throw new NotImplementedException("You need to implement this function.");
+    }
+
+    private static string Verse(int verseNumber)
+    {
+        string start = "This is the ";
+        string end = "the house that Jack build";
+
+        if (verseNumber == 1)
+            return "This is the house that Jack built.";
+        else
+            throw new NotImplementedException("to be done");
     }
 }
