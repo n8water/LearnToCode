06dba2cd3fbeebb1fdff549a6a38976c337c6e2c
Neskaya <39946966+n8water@users.noreply.github.com>
2019-11-12 20:51:43 +0100

    work in progress

--- csharp/darts/Darts.cs
+++ csharp/darts/Darts.cs
@@ -4,6 +4,25 @@ public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        double targetRadius = Math.Sqrt(x * x + y * y);
+
+        return GetScore(targetRadius);
     }
+
+    private static int GetScore(double targetRadius)
+    {
+        int score = 0;
+
+        if (targetRadius <= 1)
+            score = 10;
+        else if (targetRadius <= 5)
+            score = 5;
+        else if (targetRadius <= 10)
+            score = 1;
+        else
+            score = 0;
+
+        return score;
+    }
+
 }
