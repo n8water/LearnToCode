6735ea0a204c658daf32d76c7be01da1f073a686
neskaya <n8water@gmail.com>
2019-11-18 09:22:48 +0100

    Quickstart Notes
    Cross platform development with Xamarin.Forms
    
    https://docs.microsoft.com/en-us/xamarin/get-started/quickstarts/single-page?pivots=windows

--- Android/FirstXamarinFormsApp/Notes/Notes/MainPage.xaml.cs
+++ Android/FirstXamarinFormsApp/Notes/Notes/MainPage.xaml.cs
@@ -6,23 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace FirstXamarinFormsApp
+namespace Notes
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            ++count;
-            ((Button)sender).Text = $"You clicked {count} times.";
         }
     }
 }
