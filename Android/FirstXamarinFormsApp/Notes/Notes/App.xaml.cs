c9b5a2badac6c45b523d143228a713112172efbb
neskaya <n8water@gmail.com>
2019-11-19 09:00:01 +0100

    Changed from files to database

--- Android/FirstXamarinFormsApp/Notes/Notes/App.xaml.cs
+++ Android/FirstXamarinFormsApp/Notes/Notes/App.xaml.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.IO;
 using Xamarin.Forms;
+using Notes.Data;
 
 namespace Notes
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
 
         public App()
         {
-            InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            InitializeComponent();           
             MainPage = new NavigationPage(new NotesPage());
         }
 
---
+++ Android/FirstXamarinFormsApp/Notes/Notes/Data/NoteDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Notes.Models;
+
+namespace Notes.Data
+{
+    public class NoteDatabase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public NoteDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Note>().Wait();
+        }
+
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return database.Table<Note>().ToListAsync();
+        }
+
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return database.Table<Note>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if(note.ID != 0)
+            {
+                return database.UpdateAsync(note);
+            }
+            else 
+            {
+                return database.InsertAsync(note);
+            }
+        }
+
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return database.DeleteAsync(note);
+        }
+    }
+}
