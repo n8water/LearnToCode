b92430d5bb430c5cdb63f3b885ae21f80ffa98bb
neskaya <n8water@gmail.com>
2020-01-30 09:50:22 +0100

    Update Monkey.cs

--- DesignPatterns/Console/classes/Monkey.cs
+++ DesignPatterns/Console/classes/Monkey.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace Console.classes
 {
-    class Monkey
+    class Monkey : ILiving
     {
+        public void SetFavFood(string newFood)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetHeight(double newHeight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetName(string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSound(string newSound)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSpeed(double newSpeed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWeight(double newWeight)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
