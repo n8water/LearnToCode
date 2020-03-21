fe5e96ae3c43d28ed6c3d37df2fb82c49d52181b
Neskaya <39946966+n8water@users.noreply.github.com>
2020-03-21 23:06:11 +0100

    First solution
    Dont really like the solution with the counter variable, but it works

--- csharp/sieve/Sieve.cs
+++ csharp/sieve/Sieve.cs
@@ -7,52 +7,62 @@ public static class Sieve
     public static int[] Primes(int limit)
     {
         //Variables
-        var myArray = new int[limit];
+        var myArray = new int[limit + 1];
 
-
-        //Work
+        // Check if Limit < 2
         if(limit < 2)
             throw new ArgumentOutOfRangeException();
 
+        // Else if Limit >= 2
+        for (int i = 0; i <= limit; i++)
+        {
+            if (i < 2)
+            {
+                myArray[i] = 1;
+            }
+            else if (myArray[i] != 1)
+            {
+                SetValue(i);
+            }
+        }
+        
+        return GenerateResult(myArray);
 
 
-        //for (int i = 0; i <= limit; i++)
-        //{
-        //    if (i < 2)
-        //    {
-        //        myArray[i] = 1;
-        //    }
-        //    else if (myArray[i] != 1)
-        //    {
-        //        SetValue(i);
-        //    }
+        #region Helper Methods
+        void SetValue(int i)
+        {
+            int n = 2;
 
-        //    return GenerateResult(myArray);
-        //}
+            while (i * n <= limit)
+            {
+                myArray[i * n] = 1;
+                n++;
+            }
+        }
 
-        //throw new NotImplementedException("You need to implement this function.");
+        int[] GenerateResult(int[] input)
+        {
+            if (input is object)
+            {
+                int counter = 0;
+                var temp = input.ToList();
+                List<int> result = new List<int>();
 
-        //void SetValue(int i)
-        //{
-        //    int n = 2;
+                foreach(var t in temp)
+                {                    
+                    if(t == 0)
+                    {
+                        result.Add(counter);                        
+                    }
+                    counter++;
+                }
 
-        //    while(i * n <= limit)
-        //    {
-        //        myArray[i * n] = 1;
-        //        n++;
-        //    }
-        //}
-
-        //int[] GenerateResult(int[] input)
-        //{
-        //    if(input is object)
-        //    {
-        //        var searchList = input.ToList<int>();
-
-        //        var results = searchList.Where(x => x == 0).Select(searchList.);
-
-        //    }
-
-        throw new ArgumentNullException();
+                return result.ToArray();
+            }
+            else
+                throw new ArgumentNullException();
+        }
+        #endregion Helper Methods
     }
 }
