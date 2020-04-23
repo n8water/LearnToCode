2280a895e0efc248dd9d5032b5b5cc86126196d1
neskaya <n8water@gmail.com>
2020-04-23 13:44:22 +0200

    CSharpMasteryCourse
    Some first testing of stuff he talked about

---
+++ CSharpMasteryCourse_AngelSix/RepeatLessons/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RepeatLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Variables
+
+            var a = $"{ 400.ToString()     }";
+            var b = $"{ false.ToString() }";
+            var b1 = $"{ false }";
+            var c = true;
+
+            Console.WriteLine("a is " + a);
+            Console.WriteLine("b is " + b);
+            Console.WriteLine("b1 is " + b1);
+            Console.WriteLine("c is " + c.ToString() );
+            Console.WriteLine("c is " + c );
+
+        }
+    }
+}
---
+++ CSharpMasteryCourse_AngelSix/RepeatLessons/String.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepeatLessons
+{
+    public class String
+    {
+        // Test with string interpolation
+        
+
+    }
+}
