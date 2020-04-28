2aba8675b5fc643bac6e3a8423fc1715394ca367
neskaya <n8water@gmail.com>
2020-04-28 11:25:38 +0200

    Update Program.cs
    WhileLoop exercise from imgur
    
    https://imgur.com/a/iiyaUxU

--- CSharpMasteryCourse_AngelSix/RepeatLessons/Program.cs
+++ CSharpMasteryCourse_AngelSix/RepeatLessons/Program.cs
@@ -6,21 +6,53 @@ namespace RepeatLessons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "WhileLoop";
 
-            // Variables
+            int[] nums = new int[10];
+            int i = 0;
 
-            var a = $"{ 400.ToString()     }";
-            var b = $"{ false.ToString() }";
-            var b1 = $"{ false }";
-            var c = true;
+            while( i < nums.Length)
+            {
+                nums[i] = i;
+                if (i == 8) { Console.Write(" | Skipped"); }
+                else
+                {
+                    Console.Write(" | " + nums[i]);
+                }
+                i++;
+            }
 
-            Console.WriteLine("a is " + a);
-            Console.WriteLine("b is " + b);
-            Console.WriteLine("b1 is " + b1);
-            Console.WriteLine("c is " + c.ToString() );
-            Console.WriteLine("c is " + c );
+            Console.WriteLine("\n\n");
 
+            do
+            {
+                i--;
+                if (i == 8) { Console.Write(" | Skipped"); continue; }
+                if (i == 3) { Console.Write(" | Done"); break; }
+                Console.Write(" | " + i);
+            }
+            while (i > 0);
+            Console.ReadKey();
         }
+
+        #region repetition
+        //{
+        //    Console.WriteLine("Hello World!");
+
+        //    // Variables
+
+        //    var a = $"{ 400.ToString()     }";
+        //    var b = $"{ false.ToString() }";
+        //    var b1 = $"{ false }";
+        //    var c = true;
+
+        //    Console.WriteLine("a is " + a);
+        //    Console.WriteLine("b is " + b);
+        //    Console.WriteLine("b1 is " + b1);
+        //    Console.WriteLine("c is " + c.ToString() );
+        //    Console.WriteLine("c is " + c );
+
+        //}
+        #endregion repetition
     }
 }
