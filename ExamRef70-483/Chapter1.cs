6aadef67a4ec336900aa9141b73988da04ee4b50
n8water <39946966+n8water@users.noreply.github.com>
2020-06-05 16:40:40 +0200

    Programmin in C#
    Exam Ref 70-483 von Ingolf zum Lernen bekommen. Zertifizierung erwünscht

---
+++ ExamRef70-483/Chapter1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ExamRef70_483
+{
+    public static class Chapter1
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("Starting Task 1");
+            Thread.Sleep(2000);
+            Console.WriteLine("Ending Task 1");
+        }
+
+        public static void Task2()
+        {
+            Console.WriteLine("Starting Task 2");
+            Thread.Sleep(1000);
+            Console.WriteLine("Ending Task 2");
+        }
+
+        public static void Task3()
+        {
+            Console.WriteLine("Starting Task 3");
+            Thread.Sleep(1500);
+            Console.WriteLine("Ending Task 3");
+        }
+    }
+}
---
+++ ExamRef70-483/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace ExamRef70_483
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Parallel.Invoke(() => Chapter1.Task1(), () => Chapter1.Task2(), () => Chapter1.Task3());
+        }
+    }
+}
