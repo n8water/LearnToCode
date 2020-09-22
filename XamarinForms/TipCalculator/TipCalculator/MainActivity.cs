1653103abb8ad93bfec368b5660fa483aa5bede2
Neskaya <39946966+n8water@users.noreply.github.com>
2020-09-22 19:40:30 +0200

    TipCalculator up and running

--- XamarinForms/TipCalculator/TipCalculator/MainActivity.cs
+++ XamarinForms/TipCalculator/TipCalculator/MainActivity.cs
@@ -12,6 +12,11 @@ namespace TipCalculator
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        // field to access elements in the view (content_main.xml)
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -20,9 +25,32 @@ namespace TipCalculator
 
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+            
+            // find the view elements
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
 
-            var digitsInputEditText = FindViewById<EditText>(Resource.Id.digitsInput);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            calculateButton.Click += OnCalculateClick;
+
+
 
         }
-	}
+
+        private void OnCalculateClick(object sender, EventArgs e)
+        {
+            string input = inputBill.Text;
+            double bill = 0;
+            
+            if(double.TryParse(input, out bill))
+            {
+                var tip = bill * 0.15;
+                var total = bill + tip;
+
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString();
+            }
+        }
+    }
 }
